6fb6cf543b62b9a7ced48f20555b6336d6d1d6d6
Patrick Rotsaert <patrick.rotsaert@dioss.com>
2023-12-15 13:24:43 +0100

    Add license headers

--- src/flexfs/sftp/sftp_file.cpp
+++ src/flexfs/sftp/sftp_file.cpp
@@ -1,3 +1,10 @@
+//
+// Copyright (C) 2023 Patrick Rotsaert
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #include "flexfs/sftp/sftp_file.h"
 #include "flexfs/sftp/sftp_exceptions.h"
 #include "flexfs/core/logging.h"
--- src/flexfs/sftp/sftp_watcher.cpp
+++ src/flexfs/sftp/sftp_watcher.cpp
@@ -1,3 +1,10 @@
+//
+// Copyright (C) 2023 Patrick Rotsaert
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #include "flexfs/sftp/sftp_watcher.h"
 #include "flexfs/core/logging.h"
 #include "flexfs/core/formatters.h"
